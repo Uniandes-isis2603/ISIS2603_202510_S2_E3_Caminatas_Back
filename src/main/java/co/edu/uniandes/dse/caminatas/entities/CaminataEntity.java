1f69adbdaa14231139bd6946d841e72213aa25c7
mattiaricc05 <m.riccardi@uniandes.edu.co>
2025-02-04 09:23:52 -0500

    CaminataEntity creada

---
+++ src/main/java/co/edu/uniandes/dse/caminatas/entities/CaminataEntity.java
@@ -0,0 +1,20 @@
+package co.edu.uniandes.dse.caminatas.entities;
+
+import java.time.LocalTime;
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+
+@Data
+@Entity
+public class CaminataEntity extends BaseEntity{
+    
+    private String titulo;
+    private String tipo;
+    private Date fecha;
+    private LocalTime hora;
+    private String departamento;
+    private String ciudad;
+    private float duracionEstimadaMinutos;
+}
