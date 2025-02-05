3566d05bfc49ae47f4fde31ce9b785696cd78993
Nicolas Rey <n.reyl@uniandes.edu.co>
2025-02-05 08:34:16 -0500

    muchos a uno asociacion

--- src/main/java/co/edu/uniandes/dse/caminatas/entities/BlogEntity.java
+++ src/main/java/co/edu/uniandes/dse/caminatas/entities/BlogEntity.java
@@ -1,16 +1,17 @@
 package co.edu.uniandes.dse.caminatas.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 import java.io.File;
 
 @Entity
 @Data
-
-public class BlogEntity extends BaseEntity{
+public class BlogEntity extends BaseEntity {
     private File foto;
     private File video;
     private String text;
-    private CaminanteEntity caminante;
 
+    @ManyToOne
+    private CaminanteEntity caminante;
 }
