8200fb42efae2bd14f53dbc53bb9b72773a31649
alicia3007 <157715123+alicia3007@users.noreply.github.com>
2025-02-10 22:18:42 -0500

    Create CaminataRepository.java

---
+++ src/main/java/co/edu/uniandes/dse/caminatas/repositories/CaminataRepository.java
@@ -0,0 +1,14 @@
+package co.edu.uniandes.dse.caminatas.repositories;
+
+import java.util.List;
+import co.edu.uniandes.dse.caminatas.entities.PatrocinadorEntity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import co.edu.uniandes.dse.caminatas.entities.CaminataEntity;
+
+@Repository
+public interface CaminataRepository extends JpaRepository<CaminataEntity, Long> {
+    List<CaminataEntity> findByPatrocinador(PatrocinadorEntity patrocinador);
+}
