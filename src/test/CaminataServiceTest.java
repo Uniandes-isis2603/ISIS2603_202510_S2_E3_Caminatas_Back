3ca9871f4fa1cdcc9ae0abcd6fa2ac65840c3231
alicia3007 <as.robinson@uniandes.edu.co>
2025-02-13 12:02:47 -0500

    test caminata

--- src/test/CaminataServiceTest.java
+++ src/test/CaminataServiceTest.java
@@ -1,5 +1,63 @@
 package co.edu.uniandes.dse.caminatas.services;
 
-public class CaminataServiceTest {
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import jakarta.transaction.Transactional;
+
+public class CaminataServiceTest 
+{
+    @DataJpaTest
+    @Transactional
+    @Import(CaminataService.class)
+    public class CaminataServiceTest 
+    {
+        @Autowired
+        private CaminataService caminataService;
+
+        @Autowired
+        private TestEntityManager entityManager;
+
+        private List<CaminataEntity> caminatasList = new ArrayList<>();
+
+        @BeforeEach
+        void setUp()
+        {
+            clearData();
+            insertData();
+        }
+
+        private void clearData()
+        {
+            entityManager.getEntityManager().createQuery("delete from CaminataEntity").executeUpdate();
+        }
+
+        private void insertData()
+        {
+            CaminataEntity caminataEntity = factory.manufacturePojo(CaminataEntity.class);
+            entityManager.persist(camiantaEntity);
+            caminatasList.add(camiantaEntity);
+        }
+
+        @Test
+        void testCreateCaminata() throws EntityNotFoundException, IllegalOperationException
+        {
+            CaminataEntity caminata = factory.manufacturePojo(CaminataEntity.class);
+            CaminataEntity result = caminataService.createCaminata(caminata);
+            assertNotNull(result);
+            CaminataEntity entity = entityManager.find(CaminataEntity.class, result.getId());
+            assertNotNull(entity);
+            assertEquals(caminata.getId(), entity.getId());
+            assertEquals(caminata.getTitulo(), entity.getTitulo());
+            assertEquals(caminata.getTipo(), entity.getTipo());
+            assertEquals(caminata.getFecha(), entity.getFecha());
+            assertEquals(caminata.getHora(), entity.getHora());
+            assertEquals(caminata.getDepartamento(), entity.getDepartamento());
+            assertEquals(caminata.getCiudad(), entity.getCiudad());
+            assertEquals(caminata.getDuracionEstimadaMinutos(), entity.getDuracionEstimadaMinutos());
+        }
+
+    }
+
+
 
 }
