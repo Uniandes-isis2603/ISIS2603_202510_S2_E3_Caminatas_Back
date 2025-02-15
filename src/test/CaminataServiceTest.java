7b18d1e1d4a1bbc0dcf71a662b58c4631cd31905
alicia3007 <as.robinson@uniandes.edu.co>
2025-02-15 08:22:30 -0500

    caminataTest finalizado

--- src/test/CaminataServiceTest.java
+++ src/test/CaminataServiceTest.java
@@ -56,8 +56,246 @@ public class CaminataServiceTest
             assertEquals(caminata.getDuracionEstimadaMinutos(), entity.getDuracionEstimadaMinutos());
         }
 
+        /**
+         * Prueba para crear una caminata con un título nulo.
+         */
+
+        @Test
+        void testCreateCaminataTituloNull() throws EntityNotFoundException, IllegalOperationException
+        {
+            assertThrows(IllegalOperationException.class, () -> 
+            {
+                CaminataEntity caminata = factory.manufacturePojo(CaminataEntity.class);
+                caminata.setTitulo(null);
+                caminataService.createCaminata(caminata);
+            });
+        }
+
+        /**
+         * Prueba para crear una caminata con un título vacío.
+         */
+        @Test
+        void testCreateCaminataTituloVacio()
+        {
+            assertThrows(IllegalOperationException.class, () -> 
+            {
+                CaminataEntity caminata = factory.manufacturePojo(CaminataEntity.class);
+                caminata.setTitulo("");
+                caminataService.createCaminata(caminata);
+            });
+        }
+
+        /**
+         * Prueba para crear una caminata con un id nulo.
+         */
+        @Test
+        void testCreateCaminataIdNull() 
+        {
+            assertThrows(IllegalOperationException.class, () -> 
+            {
+                CaminataEntity caminata = factory.manufacturePojo(CaminataEntity.class);
+                caminata.setId(null);
+                caminataService.createCaminata(caminata);
+            });
+        }
+
+        /**
+         * Prueba para crear una caminata con un id cero.
+         */
+        @Test
+        void testeCreateCaminataIdCero()
+        {
+            assertThrows(IllegalOperationException.class, () ->
+            {
+                CaminataEntity caminata = factory.manufacturePojo(CaminataEntity.class);
+                caminata.setId(0);
+                caminataService.createCaminata(caminata);
+            });
+        }
+
+        /**
+         * Prueba para crear una caminata con un id ya existente.
+         */
+        @Test
+        void testCreateCaminataIdExistente()
+        {
+            assertThrows(IllegalOperationException.class, () ->
+            {
+                CaminataEntity caminata = factory.manufacturePojo(CaminataEntity.class);
+                caminata.setId(caminatasList.get(0).getId());
+                caminataService.createCaminata(caminata);
+            });
+        }
+
+        /**
+         * Prueba para crear una caminata con un tipo nulo.
+         */
+        @Test   
+        void testCreateCaminataTipoNull()
+        {
+            assertThrows(IllegalOperationException.class, () ->
+            {
+                CaminataEntity caminata = factory.manufacturePojo(CaminataEntity.class);
+                caminata.setTipo(null);
+                caminataService.createCaminata(caminata);
+            });
+        }
+
+        /**
+         * Prueba para crear una caminata con un tipo vacío.
+         */
+        @Test
+        void testCreateCaminataTipoVacio()
+        {
+            assertThrows(IllegalOperationException.class, () ->
+            {
+                CaminataEntity caminata = factory.manufacturePojo(CaminataEntity.class);
+                caminata.setTipo("");
+                caminataService.createCaminata(caminata);
+            });
+        }
+
+        /**
+         * Prueba para crear una caminata con una fecha nula.
+         */
+        @Test
+        void testCreateCaminataFechaNull()
+        {
+            assertThrows(IllegalOperationException.class, () ->
+            {
+                CaminataEntity caminata = factory.manufacturePojo(CaminataEntity.class);
+                caminata.setFecha(null);
+                caminataService.createCaminata(caminata);
+            });
+        }
+
+        /**
+         * Prueba para crear una caminata con una fecha anterior a la actual.
+         */
+        @Test
+        void testCreateCaminataFechaAnterior()
+        {
+            assertThrows(IllegalOperationException.class, () ->
+            {
+                CaminataEntity caminata = factory.manufacturePojo(CaminataEntity.class);
+                caminata.setFecha(new Date(0));
+                caminataService.createCaminata(caminata);
+            });
+        }
+
+        /**
+         * Prueba para crear una caminata con una hora nula.
+         */
+        @Test
+        void testCreateCaminataHoraNull()
+        {
+            assertThrows(IllegalOperationException.class, () ->
+            {
+                CaminataEntity caminata = factory.manufacturePojo(CaminataEntity.class);
+                caminata.setHora(null);
+                caminataService.createCaminata(caminata);
+            });
+        }
+
+        /**
+         * Prueba para crear una caminata con una hora anterior a la actual.
+         */
+        @Test
+        void testCreateCaminataHoraAnterior()
+        {
+            assertThrows(IllegalOperationException.class, () ->
+            {
+                CaminataEntity caminata = factory.manufacturePojo(CaminataEntity.class);
+                caminata.setHora(LocalTime.MIN);
+                caminataService.createCaminata(caminata);
+            });
+        }
+
+        /**
+         * Prueba para crear una caminata con un departamento nulo.
+         */
+        @Test
+        void testCreateCaminataDepartamentoNull()
+        {
+            assertThrows(IllegalOperationException.class, () ->
+            {
+                CaminataEntity caminata = factory.manufacturePojo(CaminataEntity.class);
+                caminata.setDepartamento(null);
+                caminataService.createCaminata(caminata);
+            });
+        }
+
+        /**
+         * Prueba para crear una caminata con un departamento vacío.
+         */
+        @Test
+        void testCreateCaminataDepartamentoVacio()
+        {
+            assertThrows(IllegalOperationException.class, () ->
+            {
+                CaminataEntity caminata = factory.manufacturePojo(CaminataEntity.class);
+                caminata.setDepartamento("");
+                caminataService.createCaminata(caminata);
+            });
+        }
+
+        /**
+         * Prueba para crear una caminata con un departamento que no pertenece a Colombia.
+         */
+        @Test
+        void testCreateCaminataDepartamentoNoColombia()
+        {
+            assertThrows(IllegalOperationException.class, () ->
+            {
+                CaminataEntity caminata = factory.manufacturePojo(CaminataEntity.class);
+                caminata.setDepartamento("Mérida");
+                caminataService.createCaminata(caminata);
+            });
+        }
+
+        /**
+         * Prueba para crear una caminata con una ciudad nula.
+         */
+        @Test
+        void testCreateCaminataCiudadNull()
+        {
+            assertThrows(IllegalOperationException.class, () ->
+            {
+                CaminataEntity caminata = factory.manufacturePojo(CaminataEntity.class);
+                caminata.setCiudad(null);
+                caminataService.createCaminata(caminata);
+            });
+        }
+
+        /**
+         * Prueba para crear una caminata con una ciudad vacía.
+         */
+        @Test
+        void testCreateCaminataCiudadVacia()
+        {
+            assertThrows(IllegalOperationException.class, () ->
+            {
+                CaminataEntity caminata = factory.manufacturePojo(CaminataEntity.class);
+                caminata.setCiudad("");
+                caminataService.createCaminata(caminata);
+            });
+        }
+
+        /**
+         * Prueba para crear una caminata con una duración estimada menor o igual a cero.
+         */
+        @Test
+        void testCreateCaminataDuracionEstimadaCero()
+        {
+            assertThrows(IllegalOperationException.class, () ->
+            {
+                CaminataEntity caminata = factory.manufacturePojo(CaminataEntity.class);
+                caminata.setDuracionEstimadaMinutos(0);
+                caminataService.createCaminata(caminata);
+            });
+
+        }
+
     }
-
-
 
 }
