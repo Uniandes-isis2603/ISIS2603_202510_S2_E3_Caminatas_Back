9093adbfa01f522ec821835127ccec0d600a2cf1
mattiaricc05 <m.riccardi@uniandes.edu.co>
2025-02-25 09:09:48 -0500

    SeguroDTO

---
+++ src/main/java/co/edu/uniandes/dse/caminatas/dto/SeguroDTO.java
@@ -0,0 +1,14 @@
+package co.edu.uniandes.dse.caminatas.dto;
+
+import lombok.Data;
+
+@Data
+public class SeguroDTO {
+    private Long id;
+    private Long numero;
+    private String nombre;
+    private String tipo;
+    private String cobertura;
+    private String condiciones;
+    private float costo;
+}
