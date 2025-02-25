20f263e9104af55fb5d496cc1b3419f02c365905
Juan Rodriguez <je.rodriguezd12@uniandes.edu.co>
2025-02-25 09:10:54 -0500

    PagoDTO

---
+++ src/main/java/co/edu/uniandes/dse/caminatas/dto/PagoDTO.java
@@ -0,0 +1,23 @@
+package co.edu.uniandes.dse.caminatas.dto;
+
+import java.util.Date;
+import lombok.Data;
+
+@Data
+public class PagoDTO {
+	private String numeroTransaccion;
+	private float valorCaminata;
+	private float valorTotal;
+	private String numeroTarjeta;
+	private Date fechaVencimiento;
+	private String ccv;
+    private int cuotas;
+    private String propietario;
+
+    /* 
+    private EmpresaDTO empresa;
+    private CaminanteDTO caminante;
+    private CaminataDTO caminata;
+	*/
+}
+
