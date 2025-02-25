62d42cd35638adc81436d767f2acf4fa04e6aba4
Nicolas Rey <n.reyl@uniandes.edu.co>
2025-02-25 09:10:45 -0500

    EmpresaDTO

---
+++ src/main/java/co/edu/uniandes/dse/caminatas/dto/EmpresaDTO.java
@@ -0,0 +1,10 @@
+package co.edu.uniandes.dse.caminatas.dto;
+import lombok.Data;
+
+@Data
+public class EmpresaDTO {
+
+    private String nombre;
+    private int documento;
+    private String correo;
+}
