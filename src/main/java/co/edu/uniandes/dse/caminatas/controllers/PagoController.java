380687f6d87d46f0ec3a2e162f0688a8d29b9be0
Juan Rodriguez <je.rodriguezd12@uniandes.edu.co>
2025-03-04 09:10:09 -0500

    Pago Controller

---
+++ src/main/java/co/edu/uniandes/dse/caminatas/controllers/PagoController.java
@@ -0,0 +1,40 @@
+package co.edu.uniandes.dse.caminatas.controllers;
+
+import java.util.List;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import co.edu.uniandes.dse.caminatas.dto.PagoDetailDTO;
+import co.edu.uniandes.dse.caminatas.dto.SeguroDetailDTO;
+import co.edu.uniandes.dse.caminatas.entities.PagoEntity;
+import co.edu.uniandes.dse.caminatas.services.PagoService;
+import org.modelmapper.TypeToken;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+
+@RestController
+@RequestMapping("/pagos")
+public class PagoController {
+
+    @Autowired
+    private PagoService pagoService;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @GetMapping
+    @ResponseStatus(code = HttpStatus.OK)
+    public List<PagoDetailDTO> findAll(){
+        List<PagoEntity> seguros = pagoService.getPagos();
+        return modelMapper.map(seguros, new TypeToken<List<SeguroDetailDTO>>() {
+
+        }.getType());
+    }
+    
+}
