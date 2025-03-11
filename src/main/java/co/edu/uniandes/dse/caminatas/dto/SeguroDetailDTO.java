cc81c78a1037daac3139b8ce573412e4b8bdec4a
mattiaricc05 <m.riccardi@uniandes.edu.co>
2025-03-10 20:59:42 -0500

    SeguroDetailDTO caminatas

--- src/main/java/co/edu/uniandes/dse/caminatas/dto/SeguroDetailDTO.java
+++ src/main/java/co/edu/uniandes/dse/caminatas/dto/SeguroDetailDTO.java
@@ -1,5 +1,11 @@
 package co.edu.uniandes.dse.caminatas.dto;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.Data;
+
+@Data
 public class SeguroDetailDTO extends SeguroDTO{
-    
+    private List<CaminataDTO> caminatas = new ArrayList<>();
 }
