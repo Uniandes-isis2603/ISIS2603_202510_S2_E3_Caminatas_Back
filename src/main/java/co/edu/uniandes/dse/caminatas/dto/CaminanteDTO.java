3f2a9b6ac20b7f513d7871a4bec28782b771f951
Pablo <p.ramirezt2@uniandes.edu.co>
2025-03-11 09:08:10 -0500

    Blog, Caminante y Patrocinador Collections

--- src/main/java/co/edu/uniandes/dse/caminatas/dto/CaminanteDTO.java
+++ src/main/java/co/edu/uniandes/dse/caminatas/dto/CaminanteDTO.java
@@ -2,7 +2,8 @@ package co.edu.uniandes.dse.caminatas.dto;
 
 import lombok.Data;
 @Data
-public class CaminanteDTO {   
+public class CaminanteDTO {
+    private Long id;
     private String nombre;
     private String documento;
     private String correo;
