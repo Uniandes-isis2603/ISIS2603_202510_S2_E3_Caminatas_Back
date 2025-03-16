dab8f34a620bc57731656424922729bcf0968b23
Juan Rodriguez <je.rodriguezd12@uniandes.edu.co>
2025-03-16 14:11:46 -0500

    Documentación pago service

--- src/main/java/co/edu/uniandes/dse/caminatas/services/PagoService.java
+++ src/main/java/co/edu/uniandes/dse/caminatas/services/PagoService.java
@@ -25,6 +25,22 @@ public class PagoService
     @Autowired
 	PagoRepository pagoRepository;
 
+    /**
+     * Crea una nueva Empresa en la base de datos.
+     *
+     * Reglas de negocio:
+     * - El numero de transacción no se puede repetir.
+     * - Los valores del pago no pueden ser menores a 0.
+     * - La tarjeta y el codigo CCV deben tener una cantidad de digitos especifica.
+     * - El pago debe contar con una caminata.
+     * - El pago debe contar con un usuario, ya sea una empresa o caminante.
+     * - El pago no pueden contar con dos usuarios.
+     *
+     * @param pago la entidad con los datos nuevos.
+     * @return la entidad en la base de datos guardada.
+     * @throws IllegalOperationException si se incumple alguna regla de negocio.
+     * @throws EntityNotFoundException si falta una entidad en el pago.
+     */
     @Transactional
     public PagoEntity createPago (PagoEntity pago) throws EntityNotFoundException, IllegalOperationException
     {
@@ -60,6 +76,11 @@ public class PagoService
         return pagoRepository.save(pago);
     }
 
+    /**
+     * Obtiene la lista de todas los pagos.
+     *
+     * @return Lista de PagoEntity.
+     */
     @Transactional
     public List<PagoEntity> getPagos ()
     {
@@ -68,6 +89,13 @@ public class PagoService
         return pagos;
     }
     
+    /**
+     * Obtiene los datos de un pago a partir de su ID.
+     *
+     * @param id Identificador de la instancia a consultar.
+     * @return Instancia de PagoEntity con los datos consultados.
+     * @throws EntityNotFoundException si no se encuentra el pago.
+     */
     @Transactional
     public PagoEntity getPago (Long id) throws EntityNotFoundException
     {
