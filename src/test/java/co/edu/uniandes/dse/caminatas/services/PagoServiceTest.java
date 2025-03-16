0a404fda0db05ce974abb72bf0f7fb04cac58808
Juan Rodriguez <je.rodriguezd12@uniandes.edu.co>
2025-03-16 14:13:08 -0500

    Cambio pago service test

--- src/test/java/co/edu/uniandes/dse/caminatas/services/PagoServiceTest.java
+++ src/test/java/co/edu/uniandes/dse/caminatas/services/PagoServiceTest.java
@@ -3,7 +3,6 @@ package co.edu.uniandes.dse.caminatas.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
