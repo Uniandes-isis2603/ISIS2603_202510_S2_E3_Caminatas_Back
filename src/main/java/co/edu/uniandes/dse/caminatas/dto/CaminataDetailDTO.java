616cb9a0ad2756e3e39d9b095ed5827ebd6785a1
mattiaricc05 <m.riccardi@uniandes.edu.co>
2025-03-16 20:01:56 -0500

    Solución CaminataDetailDTO

--- src/main/java/co/edu/uniandes/dse/caminatas/dto/CaminataDetailDTO.java
+++ src/main/java/co/edu/uniandes/dse/caminatas/dto/CaminataDetailDTO.java
@@ -9,7 +9,6 @@ import lombok.Data;
 
 public class CaminataDetailDTO extends CaminataDTO {
     private EmpresaDTO empresa;
-    private SeguroDTO seguro;
     private List<CaminanteDTO> caminantes = new ArrayList<>();
     private List<PagoDTO> pagos = new ArrayList<>();
     private SeguroDTO seguro;
