438d59a31c0d4a836d7142db54b16a5369b257e9
mattiaricc05 <m.riccardi@uniandes.edu.co>
2025-05-04 23:54:23 -0500

    sql

---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,114 @@
+INSERT INTO CAMINATA_ENTITY (DTYPE, id, numero, titulo, tipo, fecha, hora, departamento, ciudad, duracion_estimada_minutos)
+VALUES ('CaminataEntity', 1, 101, 'Sendero del Sol', 'Montaña', '2025-06-10', '07:00', 'Cundinamarca', 'La Calera', 180);
+
+INSERT INTO CAMINATA_ENTITY (DTYPE, id, numero, titulo, tipo, fecha, hora, departamento, ciudad, duracion_estimada_minutos)
+VALUES ('CaminataEntity', 2, 102, 'Aventura del Río', 'Río', '2025-07-15', '08:30', 'Antioquia', 'Guatapé', 120);
+
+INSERT INTO CAMINATA_ENTITY (DTYPE, id, numero, titulo, tipo, fecha, hora, departamento, ciudad, duracion_estimada_minutos)
+VALUES ('CaminataEntity', 3, 103, 'Exploración del Bosque', 'Bosque', '2025-08-20', '06:45', 'Meta', 'Villavicencio', 150);
+
+INSERT INTO CAMINATA_ENTITY (DTYPE, id, numero, titulo, tipo, fecha, hora, departamento, ciudad, duracion_estimada_minutos)
+VALUES ('CaminataEntity', 4, 104, 'Ruta del Nevado', 'Nevado', '2025-09-05', '05:30', 'Tolima', 'Murillo', 240);
+
+INSERT INTO CAMINATA_ENTITY (DTYPE, id, numero, titulo, tipo, fecha, hora, departamento, ciudad, duracion_estimada_minutos)
+VALUES ('CaminataEntity', 5, 105, 'Caminata Nocturna', 'Nocturna', '2025-10-01', '19:00', 'Boyacá', 'Villa de Leyva', 90);
+
+INSERT INTO CAMINATA_ENTITY (DTYPE, id, numero, titulo, tipo, fecha, hora, departamento, ciudad, duracion_estimada_minutos)
+VALUES ('CaminataEntity', 6, 106, 'Sendero del Viento', 'Montaña', '2025-10-15', '07:15', 'Cundinamarca', 'Sibaté', 180);
+
+INSERT INTO CAMINATA_ENTITY (DTYPE, id, numero, titulo, tipo, fecha, hora, departamento, ciudad, duracion_estimada_minutos)
+VALUES ('CaminataEntity', 7, 107, 'Cima de los Andes', 'Montaña', '2025-11-01', '06:00', 'Cundinamarca', 'La Calera', 210);
+
+INSERT INTO CAMINATA_ENTITY (DTYPE, id, numero, titulo, tipo, fecha, hora, departamento, ciudad, duracion_estimada_minutos)
+VALUES ('CaminataEntity', 8, 108, 'Exploración Subterránea', 'Cueva', '2025-11-20', '09:30', 'Santander', 'Barichara', 120);
+
+INSERT INTO CAMINATA_ENTITY (DTYPE, id, numero, titulo, tipo, fecha, hora, departamento, ciudad, duracion_estimada_minutos)
+VALUES ('CaminataEntity', 9, 109, 'Aventura del Volcán', 'Volcán', '2025-12-10', '08:00', 'Cauca', 'Popayán', 180);
+
+INSERT INTO CAMINATA_ENTITY (DTYPE, id, numero, titulo, tipo, fecha, hora, departamento, ciudad, duracion_estimada_minutos)
+VALUES ('CaminataEntity', 10, 110, 'Caminata del Lago', 'Lago', '2025-12-15', '07:45', 'Nariño', 'San Juan de Pasto', 150);
+
+INSERT INTO CAMINATA_ENTITY (DTYPE, id, numero, titulo, tipo, fecha, hora, departamento, ciudad, duracion_estimada_minutos)
+VALUES ('CaminataEntity', 11, 111, 'Aventura de los Parques', 'Parque', '2026-01-05', '10:00', 'Cundinamarca', 'Chía', 90);
+
+INSERT INTO CAMINATA_ENTITY (DTYPE, id, numero, titulo, tipo, fecha, hora, departamento, ciudad, duracion_estimada_minutos)
+VALUES ('CaminataEntity', 12, 112, 'Sendero del Río Grande', 'Río', '2026-01-15', '08:30', 'Antioquia', 'Medellín', 130);
+
+INSERT INTO CAMINATA_ENTITY (DTYPE, id, numero, titulo, tipo, fecha, hora, departamento, ciudad, duracion_estimada_minutos)
+VALUES ('CaminataEntity', 13, 113, 'Ruta del Sol', 'Montaña', '2026-02-01', '06:30', 'Boyacá', 'Tunja', 180);
+
+INSERT INTO CAMINATA_ENTITY (DTYPE, id, numero, titulo, tipo, fecha, hora, departamento, ciudad, duracion_estimada_minutos)
+VALUES ('CaminataEntity', 14, 114, 'Exploración del Desierto', 'Desierto', '2026-02-10', '07:00', 'La Guajira', 'Riohacha', 240);
+
+INSERT INTO CAMINATA_ENTITY (DTYPE, id, numero, titulo, tipo, fecha, hora, departamento, ciudad, duracion_estimada_minutos)
+VALUES ('CaminataEntity', 15, 115, 'Caminata de la Noche Estrellada', 'Nocturna', '2026-03-01', '19:30', 'Cundinamarca', 'Fómeque', 110);
+
+
+insert into EMPRESA_ENTITY (DOCUMENTO, ID, CORREO, NOMBRE) values (1, 1, 'pnunns0@tmall.com', 'Pru');
+insert into EMPRESA_ENTITY (DOCUMENTO, ID, CORREO, NOMBRE) values (2, 2, 'jhaly1@businesswire.com', 'Josepha');
+insert into EMPRESA_ENTITY (DOCUMENTO, ID, CORREO, NOMBRE) values (3, 3, 'bsavine2@ted.com', 'Burlie');
+insert into EMPRESA_ENTITY (DOCUMENTO, ID, CORREO, NOMBRE) values (4, 4, 'agarvan3@jalbum.net', 'Arabella');
+insert into EMPRESA_ENTITY (DOCUMENTO, ID, CORREO, NOMBRE) values (5, 5, 'omistry4@sitemeter.com', 'Oren');
+insert into EMPRESA_ENTITY (DOCUMENTO, ID, CORREO, NOMBRE) values (6, 6, 'awheildon5@weather.com', 'Alameda');
+insert into EMPRESA_ENTITY (DOCUMENTO, ID, CORREO, NOMBRE) values (7, 7, 'jsebastian6@sun.com', 'Julienne');
+insert into EMPRESA_ENTITY (DOCUMENTO, ID, CORREO, NOMBRE) values (8, 8, 'pbeattie7@privacy.gov.au', 'Pansy');
+insert into EMPRESA_ENTITY (DOCUMENTO, ID, CORREO, NOMBRE) values (9, 9, 'atreven8@arstechnica.com', 'Anya');
+insert into EMPRESA_ENTITY (DOCUMENTO, ID, CORREO, NOMBRE) values (10, 10, 'caiston9@diigo.com', 'Cedric');
+insert into EMPRESA_ENTITY (DOCUMENTO, ID, CORREO, NOMBRE) values (11, 11, 'tkeata@harvard.edu', 'Tate');
+insert into EMPRESA_ENTITY (DOCUMENTO, ID, CORREO, NOMBRE) values (12, 12, 'rgromleyb@mac.com', 'Rogerio');
+insert into EMPRESA_ENTITY (DOCUMENTO, ID, CORREO, NOMBRE) values (13, 13, 'obreec@edublogs.org', 'Obidiah');
+insert into EMPRESA_ENTITY (DOCUMENTO, ID, CORREO, NOMBRE) values (14, 14, 'wsinclaird@howstuffworks.com', 'Warner');
+insert into EMPRESA_ENTITY (DOCUMENTO, ID, CORREO, NOMBRE) values (15, 15, 'wbelloe@exblog.jp', 'Walt');
+
+insert into CAMINANTE_ENTITY (EXPERIENCIA_PREVIA, LESION, PROBLEMAS_RES, TRATAMIENTOS_MED, ID, CORREO, DIRECCION, DOCUMENTO, NOMBRE, TELEFONO) values (false, true, false, false, 1, 'emacgahey0@adobe.com', '3rd Floor', '413-33-0118', 'Ebony', '623-781-3431');
+insert into CAMINANTE_ENTITY (EXPERIENCIA_PREVIA, LESION, PROBLEMAS_RES, TRATAMIENTOS_MED, ID, CORREO, DIRECCION, DOCUMENTO, NOMBRE, TELEFONO) values (false, true, true, true, 2, 'alampett1@imageshack.us', 'Room 1475', '346-26-3822', 'Anestassia', '701-664-7566');
+insert into CAMINANTE_ENTITY (EXPERIENCIA_PREVIA, LESION, PROBLEMAS_RES, TRATAMIENTOS_MED, ID, CORREO, DIRECCION, DOCUMENTO, NOMBRE, TELEFONO) values (false, false, true, true, 3, 'etichner2@histats.com', 'Suite 23', '113-92-0851', 'Eddy', '821-118-6758');
+insert into CAMINANTE_ENTITY (EXPERIENCIA_PREVIA, LESION, PROBLEMAS_RES, TRATAMIENTOS_MED, ID, CORREO, DIRECCION, DOCUMENTO, NOMBRE, TELEFONO) values (true, false, true, false, 4, 'cloveard3@utexas.edu', 'Room 1017', '713-43-7737', 'Cherey', '750-168-5720');
+insert into CAMINANTE_ENTITY (EXPERIENCIA_PREVIA, LESION, PROBLEMAS_RES, TRATAMIENTOS_MED, ID, CORREO, DIRECCION, DOCUMENTO, NOMBRE, TELEFONO) values (false, false, false, false, 5, 'jshelbourne4@icio.us', 'PO Box 6097', '578-81-2898', 'Jenine', '993-559-3376');
+insert into CAMINANTE_ENTITY (EXPERIENCIA_PREVIA, LESION, PROBLEMAS_RES, TRATAMIENTOS_MED, ID, CORREO, DIRECCION, DOCUMENTO, NOMBRE, TELEFONO) values (false, false, false, true, 6, 'amallett5@t-online.de', 'Room 719', '228-55-9058', 'Ayn', '112-937-1218');
+insert into CAMINANTE_ENTITY (EXPERIENCIA_PREVIA, LESION, PROBLEMAS_RES, TRATAMIENTOS_MED, ID, CORREO, DIRECCION, DOCUMENTO, NOMBRE, TELEFONO) values (false, false, true, false, 7, 'educhant6@google.pl', 'Suite 66', '707-10-4387', 'Ellynn', '517-269-3240');
+insert into CAMINANTE_ENTITY (EXPERIENCIA_PREVIA, LESION, PROBLEMAS_RES, TRATAMIENTOS_MED, ID, CORREO, DIRECCION, DOCUMENTO, NOMBRE, TELEFONO) values (true, true, false, true, 8, 'rfratczak7@sciencedaily.com', '3rd Floor', '111-58-6377', 'Reamonn', '191-727-6479');
+insert into CAMINANTE_ENTITY (EXPERIENCIA_PREVIA, LESION, PROBLEMAS_RES, TRATAMIENTOS_MED, ID, CORREO, DIRECCION, DOCUMENTO, NOMBRE, TELEFONO) values (false, true, false, false, 9, 'rronchi8@unicef.org', 'PO Box 18238', '516-91-1377', 'Rozella', '195-248-4296');
+insert into CAMINANTE_ENTITY (EXPERIENCIA_PREVIA, LESION, PROBLEMAS_RES, TRATAMIENTOS_MED, ID, CORREO, DIRECCION, DOCUMENTO, NOMBRE, TELEFONO) values (true, false, true, false, 10, 'rrittmeyer9@imageshack.us', 'Apt 339', '276-26-4579', 'Ramsey', '132-380-0864');
+insert into CAMINANTE_ENTITY (EXPERIENCIA_PREVIA, LESION, PROBLEMAS_RES, TRATAMIENTOS_MED, ID, CORREO, DIRECCION, DOCUMENTO, NOMBRE, TELEFONO) values (true, false, false, false, 11, 'gbrailsforda@earthlink.net', '15th Floor', '275-16-2991', 'Ginelle', '623-187-7805');
+insert into CAMINANTE_ENTITY (EXPERIENCIA_PREVIA, LESION, PROBLEMAS_RES, TRATAMIENTOS_MED, ID, CORREO, DIRECCION, DOCUMENTO, NOMBRE, TELEFONO) values (true, true, true, true, 12, 'ghoflerb@about.com', 'Suite 16', '452-42-9665', 'Gwendolyn', '931-397-9954');
+insert into CAMINANTE_ENTITY (EXPERIENCIA_PREVIA, LESION, PROBLEMAS_RES, TRATAMIENTOS_MED, ID, CORREO, DIRECCION, DOCUMENTO, NOMBRE, TELEFONO) values (false, false, false, true, 13, 'ksennerc@vinaora.com', 'PO Box 47045', '333-06-4929', 'Keelby', '817-965-3385');
+insert into CAMINANTE_ENTITY (EXPERIENCIA_PREVIA, LESION, PROBLEMAS_RES, TRATAMIENTOS_MED, ID, CORREO, DIRECCION, DOCUMENTO, NOMBRE, TELEFONO) values (true, false, true, false, 14, 'bipsgraved@freewebs.com', 'Apt 1307', '824-83-0328', 'Becka', '666-844-6464');
+insert into CAMINANTE_ENTITY (EXPERIENCIA_PREVIA, LESION, PROBLEMAS_RES, TRATAMIENTOS_MED, ID, CORREO, DIRECCION, DOCUMENTO, NOMBRE, TELEFONO) values (false, false, false, true, 15, 'vreddingtone@usnews.com', 'Suite 17', '664-16-6423', 'Vivienne', '178-185-3455');
+
+insert into PAGO_ENTITY (CUOTAS, VALOR_CAMINATA, VALOR_TOTAL, CAMINATA_ID, CAMINANTE_ID, EMPRESA_ID, FECHA_VENCIMIENTO, ID, CCV, NUMERO_TARJETA, NUMERO_TRANSACCION, PROPIETARIO) values (5, 81, 58, 1, 1, 1, '2024-03-23 14:49:32', 1, 'Rafaelia', 'Winna', 'Falkner', 'Georgianne');
+insert into PAGO_ENTITY (CUOTAS, VALOR_CAMINATA, VALOR_TOTAL, CAMINATA_ID, CAMINANTE_ID, EMPRESA_ID, FECHA_VENCIMIENTO, ID, CCV, NUMERO_TARJETA, NUMERO_TRANSACCION, PROPIETARIO) values (2, 68, 39, 2, 2, 2, '2024-04-05 07:36:47', 2, 'Blisse', 'Patrice', 'Lawton', 'Layton');
+insert into PAGO_ENTITY (CUOTAS, VALOR_CAMINATA, VALOR_TOTAL, CAMINATA_ID, CAMINANTE_ID, EMPRESA_ID, FECHA_VENCIMIENTO, ID, CCV, NUMERO_TARJETA, NUMERO_TRANSACCION, PROPIETARIO) values (4, 15, 53, 3, 3, 3, '2024-09-16 09:32:22', 3, 'Heidie', 'Bekki', 'Halette', 'Marika');
+insert into PAGO_ENTITY (CUOTAS, VALOR_CAMINATA, VALOR_TOTAL, CAMINATA_ID, CAMINANTE_ID, EMPRESA_ID, FECHA_VENCIMIENTO, ID, CCV, NUMERO_TARJETA, NUMERO_TRANSACCION, PROPIETARIO) values (1, 69, 99, 4, 4, 4, '2024-06-27 01:25:54', 4, 'Fannie', 'Jeanine', 'Charis', 'Raffaello');
+insert into PAGO_ENTITY (CUOTAS, VALOR_CAMINATA, VALOR_TOTAL, CAMINATA_ID, CAMINANTE_ID, EMPRESA_ID, FECHA_VENCIMIENTO, ID, CCV, NUMERO_TARJETA, NUMERO_TRANSACCION, PROPIETARIO) values (4, 2, 94, 5, 5, 5, '2024-11-19 06:01:47', 5, 'Wendeline', 'Windy', 'Sada', 'Michaeline');
+insert into PAGO_ENTITY (CUOTAS, VALOR_CAMINATA, VALOR_TOTAL, CAMINATA_ID, CAMINANTE_ID, EMPRESA_ID, FECHA_VENCIMIENTO, ID, CCV, NUMERO_TARJETA, NUMERO_TRANSACCION, PROPIETARIO) values (6, 53, 31, 6, 6, 6, '2024-09-11 08:37:59', 6, 'Alyson', 'Suzanne', 'Dasha', 'Lorry');
+insert into PAGO_ENTITY (CUOTAS, VALOR_CAMINATA, VALOR_TOTAL, CAMINATA_ID, CAMINANTE_ID, EMPRESA_ID, FECHA_VENCIMIENTO, ID, CCV, NUMERO_TARJETA, NUMERO_TRANSACCION, PROPIETARIO) values (1, 37, 17, 7, 7, 7, '2024-08-08 15:31:53', 7, 'Marian', 'Redd', 'Austine', 'Ulrikaumeko');
+insert into PAGO_ENTITY (CUOTAS, VALOR_CAMINATA, VALOR_TOTAL, CAMINATA_ID, CAMINANTE_ID, EMPRESA_ID, FECHA_VENCIMIENTO, ID, CCV, NUMERO_TARJETA, NUMERO_TRANSACCION, PROPIETARIO) values (7, 67, 46, 8, 8, 8, '2024-04-15 17:52:18', 8, 'Niven', 'Karlotte', 'Lynna', 'Darby');
+insert into PAGO_ENTITY (CUOTAS, VALOR_CAMINATA, VALOR_TOTAL, CAMINATA_ID, CAMINANTE_ID, EMPRESA_ID, FECHA_VENCIMIENTO, ID, CCV, NUMERO_TARJETA, NUMERO_TRANSACCION, PROPIETARIO) values (4, 16, 10, 9, 9, 9, '2024-09-27 00:31:25', 9, 'Marieann', 'Washington', 'Lawton', 'Syman');
+insert into PAGO_ENTITY (CUOTAS, VALOR_CAMINATA, VALOR_TOTAL, CAMINATA_ID, CAMINANTE_ID, EMPRESA_ID, FECHA_VENCIMIENTO, ID, CCV, NUMERO_TARJETA, NUMERO_TRANSACCION, PROPIETARIO) values (2, 99, 84, 10, 10, 10, '2024-03-28 01:00:19', 10, 'Wylie', 'Jobie', 'Vinnie', 'Ophelia');
+insert into PAGO_ENTITY (CUOTAS, VALOR_CAMINATA, VALOR_TOTAL, CAMINATA_ID, CAMINANTE_ID, EMPRESA_ID, FECHA_VENCIMIENTO, ID, CCV, NUMERO_TARJETA, NUMERO_TRANSACCION, PROPIETARIO) values (6, 1, 91, 11, 11, 11, '2024-12-07 23:11:33', 11, 'Filberto', 'Jacquelyn', 'Almeta', 'Lennie');
+insert into PAGO_ENTITY (CUOTAS, VALOR_CAMINATA, VALOR_TOTAL, CAMINATA_ID, CAMINANTE_ID, EMPRESA_ID, FECHA_VENCIMIENTO, ID, CCV, NUMERO_TARJETA, NUMERO_TRANSACCION, PROPIETARIO) values (4, 99, 89, 12, 12, 12, '2024-12-27 14:21:52', 12, 'Helaine', 'Lyndell', 'Seumas', 'Samara');
+insert into PAGO_ENTITY (CUOTAS, VALOR_CAMINATA, VALOR_TOTAL, CAMINATA_ID, CAMINANTE_ID, EMPRESA_ID, FECHA_VENCIMIENTO, ID, CCV, NUMERO_TARJETA, NUMERO_TRANSACCION, PROPIETARIO) values (4, 43, 1, 13, 13, 13, '2024-04-09 02:16:45', 13, 'Odessa', 'Indira', 'Gabrielle', 'Leisha');
+insert into PAGO_ENTITY (CUOTAS, VALOR_CAMINATA, VALOR_TOTAL, CAMINATA_ID, CAMINANTE_ID, EMPRESA_ID, FECHA_VENCIMIENTO, ID, CCV, NUMERO_TARJETA, NUMERO_TRANSACCION, PROPIETARIO) values (2, 15, 52, 14, 14, 14, '2024-03-23 13:28:41', 14, 'Markus', 'Marleah', 'Arch', 'Lilllie');
+insert into PAGO_ENTITY (CUOTAS, VALOR_CAMINATA, VALOR_TOTAL, CAMINATA_ID, CAMINANTE_ID, EMPRESA_ID, FECHA_VENCIMIENTO, ID, CCV, NUMERO_TARJETA, NUMERO_TRANSACCION, PROPIETARIO) values (3, 10, 40, 15, 15, 15, '2024-04-17 12:57:24', 15, 'Ramonda', 'Kelbee', 'La verne', 'Vladamir');
+
+insert into SEGURO_ENTITY (id, numero, nombre, tipo, cobertura, condiciones, costo) values (1, 1, 'Proteccion Total', 'Accidentes Personales', 'Cobertura medica en caso de accidentes durante la caminata', 'Aplica solo para caminatas dentro del territorio nacional', 203000);
+insert into SEGURO_ENTITY (id, numero, nombre, tipo, cobertura, condiciones, costo) values (2, 2, 'Seguro de Equipaje', 'Perdida de equipo', 'Reembolso por perdida o daño del equipo deportivo', 'Debe presentarse una denuncia en un maximo de 48 horas', 122000);
+insert into SEGURO_ENTITY (id, numero, nombre, tipo, cobertura, condiciones, costo) values (3, 3, 'Cobertura Legal', 'Responsabilidad Civil', 'Cubre daños a terceros durante la caminata', 'Solo aplica en caminatas organizadas por Caminatas Colombia', 304000);
+insert into SEGURO_ENTITY (id, numero, nombre, tipo, cobertura, condiciones, costo) values (4, 4, 'Proteccion Total', 'Accidentes Personales', 'Cobertura medica en caso de accidentes durante la caminata', 'Aplica solo para caminatas dentro del territorio nacional', 203000);
+insert into SEGURO_ENTITY (id, numero, nombre, tipo, cobertura, condiciones, costo) values (5, 5, 'Seguro de Equipaje', 'Perdida de equipo', 'Reembolso por perdida o daño del equipo deportivo', 'Debe presentarse una denuncia en un maximo de 48 horas', 122000);
+insert into SEGURO_ENTITY (id, numero, nombre, tipo, cobertura, condiciones, costo) values (6, 6, 'Cobertura Legal', 'Responsabilidad Civil', 'Cubre daños a terceros durante la caminata', 'Solo aplica en caminatas organizadas por Caminatas Colombia', 304000);
+insert into SEGURO_ENTITY (id, numero, nombre, tipo, cobertura, condiciones, costo) values (7, 7, 'Proteccion Total', 'Accidentes Personales', 'Cobertura medica en caso de accidentes durante la caminata', 'Aplica solo para caminatas dentro del territorio nacional', 203000);
+insert into SEGURO_ENTITY (id, numero, nombre, tipo, cobertura, condiciones, costo) values (8, 8, 'Seguro de Equipaje', 'Perdida de equipo', 'Reembolso por perdida o daño del equipo deportivo', 'Debe presentarse una denuncia en un maximo de 48 horas', 122000);
+insert into SEGURO_ENTITY (id, numero, nombre, tipo, cobertura, condiciones, costo) values (9, 9, 'Cobertura Legal', 'Responsabilidad Civil', 'Cubre daños a terceros durante la caminata', 'Solo aplica en caminatas organizadas por Caminatas Colombia', 304000);
+
+insert into BLOG_ENTITY (CAMINANTE_ID, ID, FOTO, TEXT, TITULO, VIDEO) values (1, 1, 'engage 24/7 systems', 'Flowdesk', 'Osmosis Jones', 'Montego');
+insert into BLOG_ENTITY (CAMINANTE_ID, ID, FOTO, TEXT, TITULO, VIDEO) values (2, 2, 'morph strategic users', 'Domainer', 'Gypsy', 'G');
+insert into BLOG_ENTITY (CAMINANTE_ID, ID, FOTO, TEXT, TITULO, VIDEO) values (3, 3, 'strategize intuitive paradigms', 'Trippledex', 'Candles on Bay Street ', 'M-Class');
+insert into BLOG_ENTITY (CAMINANTE_ID, ID, FOTO, TEXT, TITULO, VIDEO) values (4, 4, 'transform sticky systems', 'Bytecard', 'Casa de mi Padre', 'LX');
+insert into BLOG_ENTITY (CAMINANTE_ID, ID, FOTO, TEXT, TITULO, VIDEO) values (5, 5, 'cultivate sticky deliverables', 'Biodex', 'Return of the Pink Panther, The', 'Coachbuilder');
+insert into BLOG_ENTITY (CAMINANTE_ID, ID, FOTO, TEXT, TITULO, VIDEO) values (6, 6, 'unleash innovative e-services', 'Asoka', 'Cose da pazzi', 'M3');
+insert into BLOG_ENTITY (CAMINANTE_ID, ID, FOTO, TEXT, TITULO, VIDEO) values (7, 7, 'harness turn-key systems', 'Zathin', 'Short Film About Killing, A (Krótki film o zabijaniu)', 'H1');
+insert into BLOG_ENTITY (CAMINANTE_ID, ID, FOTO, TEXT, TITULO, VIDEO) values (8, 8, 'streamline strategic functionalities', 'Flowdesk', 'Hackers', 'Exige');
+insert into BLOG_ENTITY (CAMINANTE_ID, ID, FOTO, TEXT, TITULO, VIDEO) values (9, 9, 'leverage plug-and-play initiatives', 'Fintone', 'Art of Getting By, The', 'Montana');
+insert into BLOG_ENTITY (CAMINANTE_ID, ID, FOTO, TEXT, TITULO, VIDEO) values (10, 10, 'generate best-of-breed infrastructures', 'Konklab', 'Werner - Beinhart!', 'Truck');
