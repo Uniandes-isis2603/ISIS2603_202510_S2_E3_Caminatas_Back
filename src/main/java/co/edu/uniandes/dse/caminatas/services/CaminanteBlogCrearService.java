bbbf7c420bf1f2dfd1a8255ee3760d42bce0de60
Sofia Vasquez <s.vasquezt@uniandes.edu.co>
2025-06-03 16:50:45 -0500

    Mantenibilidad

--- src/main/java/co/edu/uniandes/dse/caminatas/services/CaminanteBlogCrearService.java
+++ src/main/java/co/edu/uniandes/dse/caminatas/services/CaminanteBlogCrearService.java
@@ -25,84 +25,69 @@ public class CaminanteBlogCrearService {
     @Autowired
     private CaminanteRepository caminanteRepository;
 
+    private CaminanteEntity getCaminanteOrThrow(Long caminanteId) throws EntityNotFoundException {
+    return caminanteRepository.findById(caminanteId)
+            .orElseThrow(() -> new EntityNotFoundException("El caminante con id = " + caminanteId + " no existe."));
+    }
+
+    private BlogEntity getBlogOrThrow(Long blogId) throws EntityNotFoundException {
+        return blogRepository.findById(blogId)
+            .orElseThrow(() -> new EntityNotFoundException("El blog con id = " + blogId + " no existe."));
+    }
+
     @Transactional
     public BlogEntity addBlog(Long caminanteId, Long blogId) throws EntityNotFoundException {
         log.info("Inicia proceso de asociar un blog al caminante con id = {}", caminanteId);
-        Optional<CaminanteEntity> caminanteEntity = caminanteRepository.findById(caminanteId);
-        Optional<BlogEntity> blogEntity = blogRepository.findById(blogId);
-
-        if (caminanteEntity.isEmpty())
-            throw new EntityNotFoundException("El caminante con id = " + caminanteId + " no existe.");
-
-        if (blogEntity.isEmpty())
-            throw new EntityNotFoundException("El blog con id = " + blogId + " no existe.");
-
-        blogEntity.get().setCaminante(caminanteEntity.get());
+        CaminanteEntity caminante = getCaminanteOrThrow(caminanteId);
+        BlogEntity blog = getBlogOrThrow(blogId);
+        blog.setCaminante(caminante);
         log.info("Termina proceso de asociar un blog al caminante con id = {}", caminanteId);
-        return blogEntity.get();
+        return blog;
     }
 
     @Transactional
     public List<BlogEntity> getBlogs(Long caminanteId) throws EntityNotFoundException {
         log.info("Inicia proceso de consultar todos los blogs del caminante con id = {}", caminanteId);
-        Optional<CaminanteEntity> caminanteEntity = caminanteRepository.findById(caminanteId);
-        if (caminanteEntity.isEmpty())
-            throw new EntityNotFoundException("El caminante con id = " + caminanteId + " no existe.");
-
+        CaminanteEntity caminante = getCaminanteOrThrow(caminanteId);
         log.info("Termina proceso de consultar todos los blogs del caminante con id = {}", caminanteId);
-        return caminanteEntity.get().getBlogsCreados();
+        return caminante.getBlogsCreados();
     }
 
     @Transactional
     public BlogEntity getBlog(Long caminanteId, Long blogId) throws EntityNotFoundException, IllegalOperationException {
         log.info("Inicia proceso de consultar el blog con id = {} del caminante con id = {}", blogId, caminanteId);
-        Optional<CaminanteEntity> caminanteEntity = caminanteRepository.findById(caminanteId);
-        Optional<BlogEntity> blogEntity = blogRepository.findById(blogId);
+        CaminanteEntity caminante = getCaminanteOrThrow(caminanteId);
+        BlogEntity blog = getBlogOrThrow(blogId);
 
-        if (caminanteEntity.isEmpty())
-        throw new EntityNotFoundException("El caminante con id = " + caminanteId + " no existe.");
-
-        if (blogEntity.isEmpty())
-            throw new EntityNotFoundException("El blog con id = " + blogId + " no existe.");
-
-        if (!blogEntity.get().getCaminante().equals(caminanteEntity.get()))
+        if (!blog.getCaminante().equals(caminante))
             throw new IllegalOperationException("El blog no está asociado al caminante");
 
         log.info("Termina proceso de consultar el blog con id = {} del caminante con id = {}", blogId, caminanteId);
-        return blogEntity.get();
+        return blog;
     }
 
     @Transactional
     public List<BlogEntity> addBlogs(Long caminanteId, List<BlogEntity> blogs) throws EntityNotFoundException {
         log.info("Inicia proceso de reemplazar los blogs asociados al caminante con id = {}", caminanteId);
-        Optional<CaminanteEntity> caminanteEntity = caminanteRepository.findById(caminanteId);
-        if (caminanteEntity.isEmpty())
-            throw new EntityNotFoundException("El caminante con id = " + caminanteId + " no existe.");
+        CaminanteEntity caminante = getCaminanteOrThrow(caminanteId);
 
         for (BlogEntity blog : blogs) {
-            Optional<BlogEntity> blogEntity = blogRepository.findById(blog.getId());
-            if (blogEntity.isEmpty())
-                throw new EntityNotFoundException("El blog con id = " + blog.getId() + " no existe.");
+            getBlogOrThrow(blog.getId());
         }
 
-        caminanteEntity.get().setBlogsCreados(blogs);
+        caminante.setBlogsCreados(blogs);
         log.info("Finaliza proceso de reemplazar los blogs asociados al caminante con id = {}", caminanteId);
-        return caminanteEntity.get().getBlogsCreados();
+        return caminante.getBlogsCreados();
     }
 
-    @Transactional
+   @Transactional
     public List<BlogEntity> replaceBlogs(Long caminanteId, List<BlogEntity> blogs) throws EntityNotFoundException {
         log.info("Inicia proceso de actualizar el caminante con id = {}", caminanteId);
-        Optional<CaminanteEntity> caminanteEntity = caminanteRepository.findById(caminanteId);
-        if (caminanteEntity.isEmpty())
-            throw new EntityNotFoundException("El caminante con id = " + caminanteId + " no existe.");
+        CaminanteEntity caminante = getCaminanteOrThrow(caminanteId);
 
         for (BlogEntity blog : blogs) {
-            Optional<BlogEntity> b = blogRepository.findById(blog.getId());
-            if (b.isEmpty())
-                throw new EntityNotFoundException("El blog con id = " + blog.getId() + " no existe.");
-
-            b.get().setCaminante(caminanteEntity.get());
+            BlogEntity b = getBlogOrThrow(blog.getId());
+            b.setCaminante(caminante);
         }
         return blogs;
     }
@@ -110,17 +95,11 @@ public class CaminanteBlogCrearService {
     @Transactional
     public void removeBlog(Long caminanteId, Long blogId) throws EntityNotFoundException {
         log.info("Inicia proceso de borrar un blog del caminante con id = {}", caminanteId);
-        Optional<CaminanteEntity> caminanteEntity = caminanteRepository.findById(caminanteId);
-        Optional<BlogEntity> blogEntity = blogRepository.findById(blogId);
+        CaminanteEntity caminante = getCaminanteOrThrow(caminanteId);
+        BlogEntity blog = getBlogOrThrow(blogId);
 
-        if (caminanteEntity.isEmpty())
-            throw new EntityNotFoundException("El caminante con id = " + caminanteId + " no existe.");
-
-        if (blogEntity.isEmpty())
-        throw new EntityNotFoundException("El blog con id = " + blogId + " no existe.");
-
-        blogEntity.get().setCaminante(null);
-        caminanteEntity.get().getBlogsCreados().remove(blogEntity.get());
+        blog.setCaminante(null);
+        caminante.getBlogsCreados().remove(blog);
         log.info("Finaliza proceso de borrar un blog del caminante con id = {}", caminanteId);
     }
 }
